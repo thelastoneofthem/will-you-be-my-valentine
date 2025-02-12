3a691b2df54c3e57ac71c697ea4da6c4ad55e9b2
thelastoneofthem <mela.zuzka@gmail.com>
2025-02-12 20:00:39 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
@@ -12,54 +12,67 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "Nie",
+      "Jesteś pewien?",
+      "DOPRAWDY?",
+      "damn",
+      "SKARBIE NOOO...",
+      "proszę :3",
+      "naprawdę ładnie proszę:3",
+      "DLACZEGO",
+      "czemu taki poważny...?",
+      "to nie jest śmieszne:(",
+      "IGOR NOOO...",
+      "strasznie nie sigma",
       ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "mega smutno :((((((",
+      "NO BARDZO BARDZO ŁADNIE PROSZĘ :3",
+      "smuteczek :(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
+  useEffect(() => {
+    // Load the Tenor embed script
+    const script = document.createElement("script");
+    script.src = "https://tenor.com/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="Bear Kiss" />
+          <div className="my-4 text-4xl font-bold">OMGGG MEGA SIGMA :333</div>
         </>
       ) : (
         <>
-          <img
-            className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-          />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <div className="tenor-gif-embed" data-postid="10508322235233729597" data-share-method="host" data-aspect-ratio="0.835341" data-width="20%">
+            <a href="https://tenor.com/view/mohg-elden-ring-gif-10508322235233729597">Mohg Elden Ring Meme</a>
+            from <a href="https://tenor.com/search/mohg-memes">Mohg Memes</a>
+          </div>
+          <h1 className="my-4 text-4xl">Skarbie, pytanko mam... będziesz moją walentynką?:3</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Ofc:3
             </button>
             <button
               onClick={handleNoClick}
-              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+              className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Nie" : getNoButtonText()}
             </button>
           </div>
         </>
@@ -67,3 +80,4 @@ export default function Page() {
     </div>
   );
 }
+
